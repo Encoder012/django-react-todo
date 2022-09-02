f7f7b8188f4be6f4f691a737ad8cb0b7fee0d85b
Krishna Aggarwal <49637067+Encoder012@users.noreply.github.com>
2022-09-02 18:45:13 +0530

    first commit

---
+++ backend/todo/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Todo
+
+# creating TodoAdmin
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+# Register your models here.
+
+admin.site.register(Todo, TodoAdmin)
